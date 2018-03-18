9d568682e2e05788fd084a4966c78b7b93478fb7
Stoeffel <schtoeffel@gmail.com>
2018-03-18 21:49:57 +0100

    extract Elmi

---
+++ src/Elmi.hs
@@ -0,0 +1,25 @@
+module Elmi
+  ( all
+  , fromModulePath
+  ) where
+
+import qualified Data.Text as T
+import Prelude hiding (all)
+import System.Directory (getDirectoryContents)
+import System.FilePath
+       (FilePath, (<.>), (</>), dropExtension, splitDirectories,
+        takeExtension)
+
+fromModulePath :: FilePath -> FilePath
+fromModulePath modulePath
+  -- TODO find elm root (elm.json)
+ = elmStuff </> T.unpack (dasherize modulePath) <.> "elmi"
+
+elmStuff :: FilePath
+elmStuff = "elm-stuff" </> "0.19.0"
+
+dasherize :: FilePath -> T.Text
+dasherize = T.intercalate "-" . fmap T.pack . splitDirectories . dropExtension
+
+all :: IO [FilePath]
+all = filter ((==) ".elmi" . takeExtension) <$> getDirectoryContents elmStuff
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,19 +7,16 @@ import qualified Args
 import qualified Data.Aeson as Aeson
 import qualified Data.Binary as B
 import Data.Either (partitionEithers)
-import qualified Data.Text as T
 import Elm.Interface (Interface)
+import qualified Elmi
 import Subset (Subset(..))
-import System.Directory (getDirectoryContents)
-import System.FilePath
-       (FilePath, (<.>), (</>), dropExtension, splitDirectories,
-        takeExtension)
+import System.FilePath (FilePath)
 
 run :: IO ()
 run = do
   Args {infoFor} <- Args.parse
   modulePaths <- infoForWhatSubset infoFor
-  result <- traverse (B.decodeFileOrFail . modulePathToElmi) modulePaths
+  result <- traverse (B.decodeFileOrFail . Elmi.fromModulePath) modulePaths
   case partitionEithers result of
     ([], decoded) -> printJSON decoded
     (errs, []) -> print errs -- TODO exitcode
@@ -32,18 +29,4 @@ infoForWhatSubset :: Subset FilePath -> IO [FilePath]
 infoForWhatSubset subset =
   case subset of
     Subset modulePaths -> return modulePaths
-    All ->
-      filter ((==) ".elmi" . takeExtension) <$>
-      getDirectoryContents elmStuffPath
-
-modulePathToElmi :: FilePath -> FilePath
-modulePathToElmi modulePath
-  -- TODO find elm root (elm.json)
- = elmStuffPath </> T.unpack (dasherizePath modulePath) <.> "elmi"
-
-elmStuffPath :: FilePath
-elmStuffPath = "elm-stuff" </> "0.19.0"
-
-dasherizePath :: FilePath -> T.Text
-dasherizePath =
-  T.intercalate "-" . fmap T.pack . splitDirectories . dropExtension
+    All -> Elmi.all
