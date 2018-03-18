88def26037b795945bcb33fd7f9e597c83a36b6c
Stoeffel <schtoeffel@gmail.com>
2018-03-18 11:08:58 +0100

    path to elmi path

--- src/Args.hs
+++ src/Args.hs
@@ -7,12 +7,9 @@ module Args
 
 import Control.Applicative (many)
 import Data.Semigroup ((<>))
-import Data.String (IsString)
-import Data.Text as T
 import Options.Applicative
-       (Mod, OptionFields, Parser, ParserInfo, argument, execParser,
-        fullDesc, header, help, helper, idm, info, long, metavar, progDesc,
-        str)
+       (Parser, ParserInfo, argument, execParser, fullDesc, header, help,
+        helper, info, metavar, progDesc, str)
 
 data Args = Args
   { modulePaths :: [FilePath] -- TODO make this a union either all or subset
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,15 @@ import qualified Args
 import qualified Data.Aeson as Aeson
 import qualified Data.Binary as B
 import Data.Either (partitionEithers)
+import qualified Data.Text as T
 import Elm.Interface (Interface)
+import System.FilePath
+       (FilePath, (<.>), (</>), dropExtension, splitDirectories)
 
 run :: IO ()
 run = do
   Args {modulePaths} <- Args.parse -- TODO map to elmi path
-  result <- traverse B.decodeFileOrFail modulePaths
+  result <- traverse (B.decodeFileOrFail . modulePathToElmi) modulePaths
   case partitionEithers result of
     ([], decoded) -> printJSON decoded
     (errs, []) -> print errs -- TODO exitcode
@@ -20,3 +23,12 @@ run = do
 
 printJSON :: [Interface] -> IO ()
 printJSON = print . Aeson.encode
+
+modulePathToElmi :: FilePath -> FilePath
+modulePathToElmi modulePath
+  -- TODO find elm root (elm.json)
+ = "elm-stuff" </> "0.19.0" </> (T.unpack $ dasherizePath modulePath) <.> "elmi"
+
+dasherizePath :: FilePath -> T.Text
+dasherizePath =
+  T.intercalate "-" . fmap T.pack . splitDirectories . dropExtension
