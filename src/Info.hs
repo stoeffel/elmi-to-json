3dcaba17973981163d697172965f5e4810197e6b
Stoeffel <schtoeffel@gmail.com>
2018-03-19 16:37:08 +0100

    use safe exceptions

---
+++ src/Info.hs
@@ -0,0 +1,45 @@
+module Info
+  ( Info(..)
+  , for
+  ) where
+
+import Control.Exception.Safe
+import qualified Data.Aeson as Aeson
+import qualified Data.Binary as B
+import Data.Semigroup ((<>))
+import qualified Data.Text as T
+import Data.Typeable (Typeable, cast)
+import Elm.Interface (Interface)
+import qualified Elmi
+import GHC.Generics (Generic)
+
+data Info = Info
+  { moduleName :: T.Text
+  , interface :: Interface
+  } deriving (Generic)
+
+instance Aeson.ToJSON Info
+
+for :: FilePath -> IO Info
+for modulePath = do
+  result <- B.decodeFileOrFail modulePath
+  case result of
+    Right interface ->
+      return
+        Info {moduleName = Elmi.toModuleName modulePath, interface = interface}
+    Left _ -> throwM (DecodingElmiFailed modulePath)
+
+data DecodingElmiFailed =
+  DecodingElmiFailed FilePath
+  deriving (Typeable)
+
+instance Show DecodingElmiFailed where
+  show (DecodingElmiFailed path) =
+    "Couldn't decode " <> path <>
+    ". This file seems to be corrupted. Try to nuke `elm-stuff` and `elm make` again."
+
+instance Exception DecodingElmiFailed where
+  toException = toException . SomeAsyncException
+  fromException se = do
+    SomeAsyncException e <- fromException se
+    cast e
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,20 +5,12 @@ module Lib
 import Args (Args(..))
 import qualified Args
 import qualified Control.Concurrent.Async as Async
+import Control.Exception.Safe
 import qualified Data.Aeson as Aeson
-import qualified Data.Binary as B
-import qualified Data.Text as T
-import Elm.Interface (Interface)
 import qualified Elmi
-import GHC.Generics (Generic)
+import qualified Info
+import Info (Info)
 import Subset (Subset(..))
-
-data Info = Info
-  { moduleName :: T.Text
-  , interface :: Interface
-  } deriving (Generic)
-
-instance Aeson.ToJSON Info
 
 -- TODO error handling, change to ExceptT
 run :: IO ()
@@ -28,13 +20,11 @@ run = do
     case infoFor of
       All -> Elmi.all
       Subset modulePaths -> return $ Elmi.fromModulePath <$> modulePaths
-  result <- Async.mapConcurrently infoForModule modulePaths
+  result <-
+    Async.mapConcurrently Info.for modulePaths `withException`
+    (\e -> print (e :: SomeAsyncException)) `finally`
+    (return ())
   printJSON result
-
-infoForModule :: FilePath -> IO Info
-infoForModule modulePath = do
-  interface <- B.decodeFile modulePath
-  return Info {moduleName = Elmi.toModuleName modulePath, interface = interface}
 
 printJSON :: [Info] -> IO ()
 printJSON = print . Aeson.encode
