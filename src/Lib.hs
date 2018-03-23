c6fa99232107d4d1d38ff41dffd1e15d0f0b482a
Stoeffel <schtoeffel@gmail.com>
2018-03-23 14:34:32 +0100

    nicer error message

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,21 +9,26 @@ import qualified Control.Concurrent.Async as Async
 import Control.Exception.Safe (SomeException, catchAny)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy as BL
+import Data.Semigroup ((<>))
 import qualified Elmi
 import qualified Info
 import qualified Rainbow as R
 
 run :: IO ()
-run = catchAny runUnsafe onError
+run = do
+  args <- Args.parse
+  runUnsafe args `catchAny` onError args
 
-runUnsafe :: IO ()
-runUnsafe = do
-  Args {infoFor, maybeOutput} <- Args.parse
+runUnsafe :: Args -> IO ()
+runUnsafe Args {infoFor, maybeOutput} = do
   modulePaths <- Elmi.for infoFor
   result <- Aeson.encode <$> Async.mapConcurrently Info.for modulePaths
   case maybeOutput of
     Just output -> BL.writeFile output result
     Nothing -> print result
 
-onError :: SomeException -> IO ()
-onError = R.putChunkLn . R.fore R.red . R.chunk . show
+onError :: Args -> SomeException -> IO ()
+onError Args {infoFor} e =
+  mapM_
+    (R.putChunkLn . R.fore R.red . R.chunk)
+    ["elmi-to-json failed" <> " for:", show infoFor, "", show e, ""]
--- src/Subset.hs
+++ src/Subset.hs
@@ -3,9 +3,16 @@ module Subset
   , allIfEmpty
   ) where
 
+import qualified Data.List as L
+import Data.Semigroup ((<>))
+
 data Subset a
   = All
   | Subset [a]
+
+instance Show a => Show (Subset a) where
+  show All = "all"
+  show (Subset xs) = "subset " <> L.intercalate ", " (fmap show xs)
 
 allIfEmpty :: [a] -> Subset a
 allIfEmpty subset =
