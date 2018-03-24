79890e76c5c5c24f4fdcec19ba5e9c241c898ae1
Stoeffel <schtoeffel@gmail.com>
2018-03-24 23:51:08 +0100

    throw an error if we don't find a module for an elmi

--- src/Elmi.hs
+++ src/Elmi.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Elmi
   ( for
   , toModuleName
   , toModulePath
   ) where
 
+import Control.Exception.Safe (Exception)
+import qualified Control.Exception.Safe as ES
 import qualified Data.Maybe as M
+import Data.Semigroup ((<>))
 import qualified Data.Text as T
+import Data.Typeable (Typeable)
 import Elm.Json (ElmJson(..))
 import Subset (Subset(..))
 import qualified System.Directory as Dir
@@ -24,8 +30,7 @@ toModulePath sourceDirecotries path =
       if exists
         then return (dir </> toFileName path)
         else toModulePath rest path
-    [] -- TODO throw
-     -> return (toFileName path)
+    [] -> ES.throwM (ModuleNotFound path)
   where
     toFileName =
       flip F.addExtension "elm" .
@@ -52,3 +57,11 @@ removeSourceDir file dirs =
   case M.mapMaybe (FE.maybeMakeRelative file) dirs of
     (x:_) -> x
     [] -> file
+
+newtype ModuleNotFound =
+  ModuleNotFound FilePath
+  deriving (Typeable, Exception)
+
+instance Show ModuleNotFound where
+  show (ModuleNotFound path) =
+    "I didn't find a module for the interface " <> path <> "."
