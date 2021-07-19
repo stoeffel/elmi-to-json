bd18efb59d247439b362272b480e67a16a4e424e
Stoeffel <schtoeffel@gmail.com>
2021-07-19 10:27:51 +0200

    Can't find haskell stack for windows 32

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ var path = require("path");
 var packageInfo = require(path.join(__dirname, "package.json"));
 var version = packageInfo.version;
 
-var root =
-"https://github.com/stoeffel/elmi-to-json/releases/download/" +
-  version +
-  "/elmi-to-json-" +
-  version;
+var root = "https://github.com/stoeffel/elmi-to-json/releases/download/" +
+           version + "/elmi-to-json-" + version;
 
 module.exports = binwrap({
-  dirname: __dirname,
-  binaries: ["elmi-to-json"],
-  urls: {
-    "darwin-x64": root + "-osx.tar.gz",
-    "darwin-arm64": root + "-osx.tar.gz", // Apple M1 chips support x64 emulation so we can re-use it for now
-    "linux-x64": root + "-linux.tar.gz",
-    "win32-x64": root + "-win32-x64.zip",
-    "win32-ia32": root + "-win32-ia32.zip"
+  dirname : __dirname,
+  binaries : [ "elmi-to-json" ],
+  urls : {
+    "darwin-x64" : root + "-osx.tar.gz",
+    "darwin-arm64" :
+        root + "-osx.tar.gz", // Apple M1 chips support x64 emulation so we can
+                              // re-use it for now
+    "linux-x64" : root + "-linux.tar.gz",
+    "win32-x64" : root + "-win32-x64.zip"
+    // "win32-ia32": root + "-win32-ia32.zip"
   }
 });
